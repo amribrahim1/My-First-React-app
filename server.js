713d8792b2a1d08d7ab011899df631cc35930541
Amr Eraky <amribrahim1@users.noreply.github.com>
2020-09-07 12:19:58 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+// eslint-disable-next-line no-unused-vars
+// const bodyParser = require('body-parser');
+const path = require("path");
 const app = express();
-const PORT = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, "build")));
 
-app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'public/index.html'));
-});
+// This route serves the React app
+app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, "build", "public/index.html")));
 
-app.listen(PORT);
+app.listen(port, () => console.log(`Server listening on port ${port}`))
